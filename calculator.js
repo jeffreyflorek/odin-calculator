035cd100f2a2289b23ee340755488e24f652bf69
Jeff Florek <jkflorek@gmail.com>
2022-01-25 07:45:52 -0500

    Add operator function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,16 @@ function multiply(factor1, factor2) {
 function divide(dividend, divisor) {
   return dividend / divisor;
 }
+
+function operate(operator, operand1, operand2) {
+  switch (operator) {
+    case '+':
+      return add(operand1, operand2);
+    case '-':
+      return subtract(operand1, operand2);
+    case '*':
+      return multiply(operand1, operand2);
+    case '/':
+      return divide(operand1, operand2);
+  }
+}
