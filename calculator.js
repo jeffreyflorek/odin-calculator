29bbb07d5d649894c54af10f3022b9cf30caca5b
Jeff Florek <jkflorek@gmail.com>
2022-01-25 07:40:27 -0500

    Add math functions

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add(addend1, addend2) {
+  return addend1 + addend2;
+}
+
+function subtract(minuend, subtrahend) {
+  return minuend - subtrahend;
+}
+
+function multiply(factor1, factor2) {
+  return factor1 * factor2;
+}
+
+function divide(dividend, divisor) {
+  return dividend / divisor;
+}
